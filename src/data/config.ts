7bba2496ce35e6c20a77ea8fcaaaeecee0422fd7
Omair Malic <omairmalic28@gmail.com>
2025-11-30 05:14:20 +0800

    Revert title

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: "Omair Malic",
-  title: "Python Automation & Web Scraper | FastAPI Developer",
+  title: "Freelance Python Expert & Fullstack Web Developer",
   tagline: "Building powerful automation tools and modern web applications",
   email: "omairmalic28@gmail.com",
   location: "Philippines",
